887c5c5f629a53acda59084eb1166632f9864232
unknown <nileemashah@yahoo.com>
2022-10-05 16:37:40 -0400

    added

--- pages/logout.js
+++ pages/logout.js
@@ -1,6 +1,34 @@
 import { logout } from "../components/auth";
+import { useRouter } from "next/router";
+import { Button } from "reactstrap";
 
 const Logout = () => {
-    return logout();
+    const router = useRouter();
+    return (
+        <>
+        <h3>Do you want to Sign Out?
+        </h3>
+        <br></br>
+        <Button
+            style={{ float: "center", width: 120 }}
+            color="primary"
+            onClick={() => {
+                logout()
+            }}
+        >
+            Yes
+        </Button>
+        <Button
+            style={{ float: "center", width: 120 }}
+            color="primary"
+            onClick={() => {
+                router.push("/")
+            }}
+        >
+            No
+        </Button>
+       
+        </>
+    );
 }
 export default Logout;
